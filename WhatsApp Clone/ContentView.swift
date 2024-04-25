7b81fee87564baa87a8d6101582b99f5549aea6e
AshishAcharya083 <imashish083@gmail.com>
2024-04-25 22:29:28 +0545

    Initial Commit

---
+++ WhatsApp Clone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WhatsApp Clone
+//
+//  Created by Ashish Acharya  on 4/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WhatsApp Clone/WhatsApp_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsApp_CloneApp.swift
+//  WhatsApp Clone
+//
+//  Created by Ashish Acharya  on 4/25/24.
+//
+
+import SwiftUI
+
+@main
+struct WhatsApp_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
