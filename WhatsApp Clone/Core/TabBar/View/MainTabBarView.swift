0781f6eb3d907d1fd978f5ea3b5df6cf61f65b3c
AshishAcharya083 <imashish083@gmail.com>
2024-04-27 10:11:00 +0545

    add required files

---
+++ WhatsApp Clone/Core/TabBar/View/MainTabBarView.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabBarView.swift
+//  WhatsApp Clone
+//
+//  Created by Ashish Acharya  on 4/25/24.
+//
+
+import Foundation
+import SwiftUI
+
+
+struct MainTabBarView : View{
+    var body: some View{
+        ZStack{
+            
+        }
+    }
+}
