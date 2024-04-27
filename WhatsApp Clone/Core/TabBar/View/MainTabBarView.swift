e8e484f9e28bef4e3071dddbd411ef88bdce789e
AshishAcharya083 <imashish083@gmail.com>
2024-04-27 13:53:28 +0545

    create tab view for home page

--- WhatsApp Clone/Core/TabBar/View/MainTabBarView.swift
+++ WhatsApp Clone/Core/TabBar/View/MainTabBarView.swift
@@ -10,9 +10,46 @@ import SwiftUI
 
 
 struct MainTabBarView : View{
+    
+    @State private var selectedIndex : Int = 0
+    
     var body: some View{
-        ZStack{
-            
+
+        TabView{
+            Text("Inbox")
+                .tabItem {
+                    VStack{
+                        Image(systemName: "bubble")
+                        Text("Inbox")
+                    }.environment(\.symbolVariants , selectedIndex == 0 ? .fill : .none )
+                }.onAppear{
+                    selectedIndex = 0
+                }
+            Text("Updates")
+                .tabItem {
+                    VStack{
+                        Image(systemName: "dial.low")
+                        Text("Updates")
+                    }.environment(\.symbolVariants , selectedIndex == 1 ? .fill : .none )
+                }.onAppear{
+                    selectedIndex = 1
+                }
+            Text("Communities")
+                .tabItem {
+                    VStack{
+                        Image(systemName: "person.3")
+                        Text("Communities")
+                    }.environment(\.symbolVariants , selectedIndex == 2 ? .fill : .none )
+                }.onAppear{
+                    selectedIndex = 2
+                }
         }
+
+           
+        
     }
+}
+
+#Preview {
+    MainTabBarView()
 }
